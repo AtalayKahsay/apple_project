db9e4f44930f6986de4a7b7a4830c68cd8ef145a
AtalayKahsay <atalay.kahsay-ug@aau.edu.et>
2026-05-28 13:56:01 -0700

    Updated navigation to React Router Link

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import searchIcon from '../../images/icons/search-icon.png'
 import cart from '../../images/icons/cart.png'
 
 import './Header.css'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
@@ -11,16 +12,16 @@ function Header() {
         <div className="internal-wrapper"> 
           <div className="header-links-wrapper">
             <ul>
-              <li className="logo-link"><a href="/"><img src={logo} /></a></li>
-              <li><a href="/mac/">Mac</a></li>
-              <li><a href="/iphone/">iphone</a></li>
-              <li><a href="/ipad/">ipad</a></li>
-              <li><a href="/watch/">Watch</a></li>
+              <li className="logo-link"><Link href="/"><img src={logo} /></Link></li>
+              <li><Link href="/mac/">Mac</Link></li>
+              <li><Link href="/iphone/">iphone</Link></li>
+              <li><Link href="/ipad/">ipad</Link></li>
+              <li><Link href="/watch/">Watch</Link></li>
               <li><a href="/tv/">TV</a></li>
-              <li><a href="/music/">Music</a></li>
-              <li><a href="/support/">Support</a></li>
-              <li className="search-link"><a href="/search/"><img src={searchIcon} /></a></li>
-              <li className="cart-link"><a  href="/cart/"><img src={cart} /></a></li>
+              <li><Link href="/music/">Music</Link></li>
+              <li><Link href="/support">Support</Link></li>
+              <li className="search-link"><Link href="/search/"><img src={searchIcon} /></Link></li>
+              <li className="cart-link"><link  href="/cart/"><img src={cart} /></link></li>
             </ul>					
           </div>
         </div>
